30bd415ba78128816c1dd05e105bbf6a56551404
Vaibhav <52103891+Martian-dev@users.noreply.github.com>
2025-04-30 15:08:46 +0530

    some changes i dont remember

--- main.cpp
+++ main.cpp
@@ -1,30 +1,51 @@
 #include "raylib.h"
 #include <iostream>
-#include <random>
 
 const int WIDTH = 1000;
 const int HEIGHT = WIDTH/16*10;
 const int BALL_RADIUS = 10;
-const int PADDLE_WIDTH = 25;
+const int PADDLE_WIDTH = 20;
 const int PADDLE_HEIGHT = 100;
+const int BALL_SPEED = 8;
+const int PADDLE_SPEED = 7;
+
+Color Blue = Color{73, 106, 164, 255};
+Color Dark_Blue = Color{65, 91, 138, 255};
+Color Light_Blue = Color{109, 143, 191, 150};
 
 int player_score = 0;
 int cpu_score = 0;
 
+enum class CollisionType {
+    None,
+    Top,
+    Middle,
+    Bottom
+};
+
+struct Contact {
+    CollisionType type;
+    float penetration;
+};
+
 class Ball{
 public:
-    int x, y;
-    int x_vel, y_vel;
+    double x, y;
+    double x_vel, y_vel;
     int radius;
 
-    Ball(double x, double y, int x_vel, int y_vel, int r): x(x), y(y), x_vel(x_vel), y_vel(y_vel), radius(r) {}
+    Ball(double x, double y, double x_vel, double y_vel, int r): x(x), y(y), x_vel(x_vel), y_vel(y_vel), radius(r) {}
 
-    void setXDirection(int xDir) {
+    void collideWithPaddle(Contact const& contact) {
+        x += contact.penetration;
+        x_vel *= -1;
 
-    }
-
-    void setYDirection() {
-
+        if (contact.type == CollisionType::Top) {
+            y_vel = -0.75 * BALL_SPEED;
+        }
+        else if (contact.type == CollisionType::Bottom) {
+            y_vel = 0.75 * BALL_RADIUS;
+        }
     }
 
     void checkScore() {
@@ -47,13 +68,13 @@ public:
         }
     }
 
-    void draw() {
-        DrawCircle(x, y, radius, LIGHTGRAY);
+    void draw() const {
+        DrawCircle(x, y, radius, WHITE);
     }
 
     void resetBall() {
-        x = GetScreenWidth()/2;
-        y = GetScreenHeight()/2;
+        x = WIDTH/2;
+        y = HEIGHT/2;
 
         int randSpeed[2] = {-1, 1};
         x_vel *= randSpeed[GetRandomValue(0, 1)];
@@ -68,16 +89,16 @@ protected:
             y = 0;
         }
 
-        if (y + height >= GetScreenHeight()) {
-            y = GetScreenHeight() - height;
+        if (y + height >= HEIGHT) {
+            y = HEIGHT - height;
         }
     }
 public:
-    int x, y, width, height;
-    int yVelocity;
+    double x, y, width, height;
+    double yVelocity;
 
-    Paddle(int x, int y, int w, int h) : x(x), y(y), width(w), height(h) {
-        yVelocity = 7;
+    Paddle(double x, double y, double w, double h) : x(x), y(y), width(w), height(h) {
+        yVelocity = PADDLE_SPEED;
     }
 
     void move() {
@@ -95,14 +116,14 @@ public:
     }
 
     void draw() {
-        DrawRectangle(x, y, width, height, WHITE);
+        DrawRectangle(x, y, width, height, LIGHTGRAY);
 
     }
 };
 
 class CpuPaddle : public Paddle {
 public:
-    CpuPaddle(int x, int y, int w, int h): Paddle(x, y, w, h) {}
+    CpuPaddle(double x, double y, double w, double h): Paddle(x, y, w, h) {}
 
     void move(int ball_y) {
         if (y + height/2 > ball_y) {
@@ -115,12 +136,52 @@ public:
     }
 };
 
-Ball ball(WIDTH/2, HEIGHT/2, 8, 8, BALL_RADIUS);
-Paddle player(WIDTH-PADDLE_WIDTH -5, HEIGHT/2 - PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT);
-CpuPaddle comp(5, HEIGHT/2 - PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT);
+Ball ball(WIDTH/2, HEIGHT/2, BALL_SPEED, 0, BALL_RADIUS);
+Paddle player(WIDTH-PADDLE_WIDTH -10, HEIGHT/2 - PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT);
+CpuPaddle comp(10, HEIGHT/2 - PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT);
 
 
 void UpdateDrawFrame(void);     // Update and Draw one frame
+
+Contact CheckPaddleCollision(Ball const& b, Paddle const& p) {
+    double ballright = b.x - b.radius;
+    double ballleft = b.x + b.radius;
+    double balltop = b.y - b.radius;
+    double ballbottom = b.y + b.radius;
+
+    double paddleleft = p.x - p.width;
+    double paddleright = p.x + 2*p.width;
+    double paddletop = p.y;
+    double paddlebottom = p.y + p.height;
+
+    Contact contact{};
+
+    if (ballright < paddleleft)
+        return contact;
+    if (ballleft > paddleright)
+        return contact;
+    if (balltop > paddlebottom)
+        return contact;
+    if (ballbottom < paddletop)
+        return contact;
+
+    double paddleUpperRange = paddletop + (p.height/3);
+    double paddleLowerRange = paddlebottom - (p.height/3);
+
+    if (b.x_vel < 0 ) // left paddle
+        contact.penetration = paddleright - ballleft;
+    else if (b.x_vel > 0) // right paddle
+        contact.penetration = paddleleft - ballright;
+
+    if ((balltop >= paddletop) && (ballbottom <= paddleUpperRange))
+        contact.type = CollisionType::Top;
+    else if ((balltop >= paddleLowerRange) && (ballbottom <= paddlebottom))
+        contact.type = CollisionType::Bottom;
+    else
+        contact.type = CollisionType::Middle;
+
+    return contact;
+}
 
 int main()
 {
@@ -148,31 +209,48 @@ void UpdateDrawFrame(void)
     comp.move(ball.y);
 
     // checking for collision
-    if (CheckCollisionCircleRec(Vector2{static_cast<float>(ball.x),
-                                        static_cast<float>(ball.y)},
-                                ball.radius,
-                                Rectangle{static_cast<float>(comp.x),
-                                          static_cast<float>(comp.y),
-                                          static_cast<float>(comp.width),
-                                          static_cast<float>(comp.height)})) {
-        ball.x_vel *= -1;
+//    if (CheckCollisionCircleRec(Vector2{static_cast<float>(ball.x),
+//                                        static_cast<float>(ball.y)},
+//                                ball.radius,
+//                                Rectangle{static_cast<float>(comp.x),
+//                                          static_cast<float>(comp.y),
+//                                          static_cast<float>(comp.width),
+//                                          static_cast<float>(comp.height)})) {
+//        ball.x_vel *= -1;
+//    }
+//    if (CheckCollisionCircleRec(Vector2{static_cast<float>(ball.x),
+//                                        static_cast<float>(ball.y)},
+//                                ball.radius,
+//                                Rectangle{static_cast<float>(player.x),
+//                                          static_cast<float>(player.y),
+//                                          static_cast<float>(player.width),
+//                                          static_cast<float>(player.height)})) {
+//        ball.x_vel *= -1;
+//    }
+
+    if (
+            Contact contact = CheckPaddleCollision(ball, player);
+            contact.type != CollisionType::None
+            ) {
+        ball.collideWithPaddle(contact);
     }
-    if (CheckCollisionCircleRec(Vector2{static_cast<float>(ball.x),
-                                        static_cast<float>(ball.y)},
-                                ball.radius,
-                                Rectangle{static_cast<float>(player.x),
-                                          static_cast<float>(player.y),
-                                          static_cast<float>(player.width),
-                                          static_cast<float>(player.height)})) {
-        ball.x_vel *= -1;
+    if (
+            Contact contact = CheckPaddleCollision(ball, comp);
+            contact.type !=  CollisionType::None
+            ) {
+        ball.collideWithPaddle(contact);
     }
+
     ball.checkScore();
+    std::cout << ball.y_vel << ball.x_vel << std::endl;
 
     // Draw
     BeginDrawing();
 
-    ClearBackground(BLACK);
-    DrawLine(WIDTH/2, 0, WIDTH/2, HEIGHT, GRAY);
+    ClearBackground(Blue);
+    DrawRectangle(WIDTH/2, 0, WIDTH/2, HEIGHT, Dark_Blue);
+    DrawCircle(WIDTH/2, HEIGHT/2, HEIGHT/3, Light_Blue);
+    DrawLine(WIDTH/2, 0, WIDTH/2, HEIGHT, LIGHTGRAY);
     ball.draw();
     player.draw();
     comp.draw();
@@ -182,4 +260,4 @@ void UpdateDrawFrame(void)
     EndDrawing();
 }
 
-// TODO: fix the collision bug, make it more playable, end screen and start screen, maybe levels, and in future maybe multiplayer.
+// TODO: Random ball spawning, delay after scoring, make it more playable, end screen and start screen, maybe levels, and in future maybe multiplayer.
